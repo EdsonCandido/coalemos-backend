99df384271594f6e26a955e9bdb530fba1740e98
Edson Cândido <edson.c.a.j@hotmail.com>
2025-07-20 00:11:02 -0300

    feat: complete creation of clientes table with all necessary fields

--- init.sql
+++ init.sql
@@ -16,7 +16,23 @@ CREATE TABLE api.usuarios(
     updated_at timestamp DEFAULT NULL
 );
 
-create table api.clientes();
+create table api.clientes(
+	cod SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	cpf varchar(11) DEFAULT NULL,
+	telefone varchar(15) DEFAULT NULL,
+	email VARCHAR(100) DEFAULT NULL,
+	cep varchar(8) DEFAULT NULL,
+	logradouro VARCHAR(255) DEFAULT NULL,
+	numero VARCHAR(10) DEFAULT NULL,
+	complemento VARCHAR(255) DEFAULT NULL,
+	bairro VARCHAR(100) DEFAULT NULL,
+	cidade VARCHAR(100) DEFAULT NULL,
+	estado VARCHAR(2) DEFAULT NULL,
+	is_ativo boolean DEFAULT true,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+	updated_at timestamp DEFAULT NULL
+);
 
 
 /**

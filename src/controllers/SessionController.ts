4798e1256f544151978e019513f8cc0a995b3338
Edson Cândido <edson.c.a.j@hotmail.com>
2025-09-19 22:43:03 -0300

    [feat] refresh token controller

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
-import logger from "../middlewares/logger";
-import { ChangePasswordService } from "../services/session/ChangePasswordService";
-import { LoginService } from "../services/session/LoginService";
+import { Request, Response } from 'express';
+import logger from '../middlewares/logger';
+import { ChangePasswordService } from '../services/session/ChangePasswordService';
+import { LoginService } from '../services/session/LoginService';
+import { RefreshTokenService } from '../services/session/RefreshTokenService';
 
 export class SessionController {
   public async login(req: Request, res: Response): Promise<Response> {
@@ -18,7 +19,7 @@ export class SessionController {
   public async recoverPassword(req: Request, res: Response): Promise<Response> {
     try {
       const { login } = req.body;
-      return res.status(200).json({ message: "Password recovery initiated" });
+      return res.status(200).json({ message: 'Password recovery initiated' });
     } catch (err) {
       logger.error(err);
       return res.status(400).send((err as Error).message);
@@ -40,9 +41,20 @@ export class SessionController {
       return res.status(400).send((err as Error).message);
     }
   }
+  public async refreshToken(req: Request, res: Response): Promise<Response> {
+    try {
+      const { refreshToken } = req.body;
+      const service = new RefreshTokenService();
+      const output = await service.execute(refreshToken);
+      return res.status(200).json(output);
+    } catch (err) {
+      logger.error(err);
+      return res.status(400).send((err as Error).message);
+    }
+  }
   public async logout(req: Request, res: Response): Promise<Response> {
     try {
-      return res.status(200).json({ message: "Logged out successfully" });
+      return res.status(200).json({ message: 'Logged out successfully' });
     } catch (err) {
       logger.error(err);
       return res.status(400).send((err as Error).message);

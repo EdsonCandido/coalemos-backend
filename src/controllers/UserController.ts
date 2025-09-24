d6add522d0415240236af8ca400febbdceff4719
Edson Cândido <edson.c.a.j@hotmail.com>
2025-09-24 00:39:19 -0300

    [refactor] validation if login by user exist

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,46 +1,52 @@
-import { Request, Response } from "express";
-import logger from "../middlewares/logger";
-import { FindUserByCodService } from "../services/user/FindUserByCodService";
-import { StoreUserService } from "../services/user/StoreUserService";
-import { ChangeActiveUserByCodService } from "../services/user/ChangeActiveUserByCodService";
+import { Request, Response } from 'express';
+import logger from '../middlewares/logger';
+import { FindUserByCodService } from '../services/user/FindUserByCodService';
+import { StoreUserService } from '../services/user/StoreUserService';
+import { ChangeActiveUserByCodService } from '../services/user/ChangeActiveUserByCodService';
 
 export class UserController {
-    public async store(req: Request, res: Response): Promise<Response> {
-        try {
-            const { cpf, login, nome, cod, senha, is_admin, is_primeiro_acesso } = req.body;
-            const service = new StoreUserService();
-            const output = await service.execute({
-                cpf, login, nome, cod,
-                senha, is_admin, is_primeiro_acesso,
-            });
-            return res.status(201).json(output);
-        } catch (err) {
-            logger.error(err);
-            return res.status(400).send((err as Error).message);
-        }
+  public async store(req: Request, res: Response): Promise<Response> {
+    try {
+      const { cpf, login, nome, cod, senha, is_admin, is_primeiro_acesso } =
+        req.body;
+      const service = new StoreUserService();
+      const output = await service.execute({
+        cpf,
+        login,
+        nome,
+        cod,
+        senha,
+        is_admin,
+        is_primeiro_acesso,
+      });
+      return res.status(200).json(output);
+    } catch (err) {
+      logger.error(err);
+      return res.status(400).send((err as Error).message);
     }
+  }
 
-    public async getUserProfile(req: Request, res: Response): Promise<Response> {
-        try {
-            const cod = req.params.id;
-            const service = new FindUserByCodService();
-            const output = await service.execute(parseInt(cod));
-            return res.status(200).json(output);
-        } catch (err) {
-            logger.error(err);
-            return res.status(400).send((err as Error).message);
-        }
+  public async getUserProfile(req: Request, res: Response): Promise<Response> {
+    try {
+      const cod = req.query.id as string;
+      const service = new FindUserByCodService();
+      const output = await service.execute(parseInt(cod));
+      return res.status(200).json(output);
+    } catch (err) {
+      logger.error(err);
+      return res.status(400).send((err as Error).message);
     }
+  }
 
-    public async changeActive(req: Request, res: Response): Promise<Response> {
-        try {
-            const cod = req.params.id;
-            const service = new ChangeActiveUserByCodService();
-            const output = await service.execute(parseInt(cod));
-            return res.status(200).json(output);
-        } catch (err) {
-            logger.error(err);
-            return res.status(400).send((err as Error).message);
-        }
+  public async changeActive(req: Request, res: Response): Promise<Response> {
+    try {
+      const cod = req.params.id;
+      const service = new ChangeActiveUserByCodService();
+      const output = await service.execute(parseInt(cod));
+      return res.status(200).json(output);
+    } catch (err) {
+      logger.error(err);
+      return res.status(400).send((err as Error).message);
     }
+  }
 }

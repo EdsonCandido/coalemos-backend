a9c7c2800eb6dca20f71924808425e5983a4f4e7
Edson Cândido <edson.c.a.j@hotmail.com>
2025-09-24 00:39:33 -0300

    [fix]

--- src/services/user/FindUserByCodService.ts
+++ src/services/user/FindUserByCodService.ts
@@ -1,42 +1,31 @@
-import { Knex } from "knex";
-import pgConnection from "../../databases/pgConnection";
+import { Knex } from 'knex';
+import pgConnection from '../../databases/pgConnection';
 
 export class FindUserByCodService {
-    private conn: Knex<any, unknown>
-    constructor() {
-        this.conn = pgConnection;
-    }
-    async execute(cod: number) {
+  private conn: Knex<any, unknown>;
+  constructor() {
+    this.conn = pgConnection;
+  }
+  async execute(cod?: number) {
+    const query = this.conn('coalemos.usuarios')
+      .where({ cod })
+      .first(
+        'cod',
+        'nome',
+        'cpf',
+        'login',
+        'foto_perfil',
+        'is_primeiro_acesso',
+        'is_admin',
+        'is_ativo',
+        'created_at',
+        'updated_at',
+      );
 
-        if(cod){
-            return await this.conn('coalemos.usuarios')
-            .where({ cod })
-            .first(
-                'cod',
-                'nome',
-                'cpf',
-                'login',
-                'foto_perfil',
-                'is_primeiro_acesso',
-                'is_admin',
-                'is_ativo',
-                'created_at',
-                'updated_at'
-            );
-        }else{
-             return await this.conn('coalemos.usuarios')
-            .first(
-                'cod',
-                'nome',
-                'cpf',
-                'login',
-                'foto_perfil',
-                'is_primeiro_acesso',
-                'is_admin',
-                'is_ativo',
-                'created_at',
-                'updated_at'
-            ); 
-        } 
-    }
+    if (cod) query.where('cod', cod).first();
+
+    const result = await query;
+
+    return result;
+  }
 }
--- src/services/user/StoreUserService.ts
+++ src/services/user/StoreUserService.ts
@@ -1,67 +1,78 @@
-import { Knex } from "knex";
-import pgConnection from "../../databases/pgConnection";
-import { string } from "zod";
-import { FindUserByCodService } from "./FindUserByCodService";
-import { dtHoje } from "../../helpers/time";
-import { hash } from "bcryptjs";
+import { Knex } from 'knex';
+import pgConnection from '../../databases/pgConnection';
+import { string } from 'zod';
+import { FindUserByCodService } from './FindUserByCodService';
+import { dtHoje } from '../../helpers/time';
+import { hash } from 'bcryptjs';
 
 type tProps = {
-    cod?: number;
-    nome: string;
-    cpf: string;
-    login: string;
-    senha?: string;
-    is_primeiro_acesso?: boolean;
-    is_admin?: boolean;
-    is_ativo?: boolean;
-    created_at?: string,
-    updated_at?: string,
-}
+  cod?: number;
+  nome: string;
+  cpf: string;
+  login: string;
+  senha?: string;
+  is_primeiro_acesso?: boolean;
+  is_admin?: boolean;
+  is_ativo?: boolean;
+  created_at?: string;
+  updated_at?: string;
+};
 export class StoreUserService {
-    private conn: Knex<any, unknown>
-    constructor() {
-        this.conn = pgConnection;
+  private conn: Knex<any, unknown>;
+  constructor() {
+    this.conn = pgConnection;
+  }
+
+  public async execute({
+    cpf,
+    login,
+    nome,
+    cod,
+    is_admin = false,
+    is_ativo = true,
+    is_primeiro_acesso = true,
+    senha,
+  }: tProps) {
+    const hoje = dtHoje();
+    const payload: tProps = {
+      cod: cod,
+      nome: nome,
+      cpf: cpf,
+      login: login,
+      is_primeiro_acesso: is_primeiro_acesso,
+      is_admin: is_admin,
+      is_ativo: is_ativo,
+      created_at: hoje,
+      updated_at: hoje,
+    };
+
+    const userExist = await this.conn('coalemos.usuarios')
+      .where('login', login)
+      .first('cod');
+
+    if (userExist && !cod) throw new Error('Usuário já cadastrado');
+
+    if (senha) {
+      payload.senha = await hash(senha, 10);
     }
 
-    public async execute({ cpf, login, nome, cod, is_admin = false, is_ativo = true,
-        is_primeiro_acesso = true, senha, }: tProps) {
+    if (cod) {
+      delete payload.cod;
+      delete payload.created_at;
 
-        const hoje = dtHoje();
-        const payload: tProps = {
-            cod: cod,
-            nome: nome,
-            cpf: cpf,
-            login: login,
-            is_primeiro_acesso: is_primeiro_acesso,
-            is_admin: is_admin,
-            is_ativo: is_ativo,
-            created_at: hoje,
-            updated_at: hoje,
-        }
+      await this.conn('coalemos.usuarios').where({ cod: cod }).update(payload);
+    } else {
+      delete payload.cod;
+      delete payload.updated_at;
 
-        if (senha) {
-            payload.senha = await hash(senha, 10);
-        }
-
-        if (cod) {
-            delete payload.cod;
-            delete payload.created_at;
-
-            await this.conn('coalemos.usuarios')
-                .where({ cod: cod })
-                .update(payload);
-        } else {
-            delete payload.cod;
-            delete payload.updated_at;
-
-            const [insert] = await this.conn('coalemos.usuarios')
-                .insert(payload).returning('cod');
-            cod = insert
-        }
-
-        const service = new FindUserByCodService();
-        const result = await service.execute(Number(cod));
-        return result;
-
+      const [insert] = await this.conn('coalemos.usuarios')
+        .insert(payload)
+        .returning('cod');
+      cod = insert.cod;
     }
+
+    const service = new FindUserByCodService();
+    const result = await service.execute(Number(cod));
+    return result;
+  }
 }

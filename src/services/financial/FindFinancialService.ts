d4c022578809069b121c116c307db83d5369b2fc
Edson Cândido <edson.c.a.j@hotmail.com>
2025-10-25 00:34:09 -0300

    [fix] metodo de listar as contas completo

--- src/services/financial/FindFinancialService.ts
+++ src/services/financial/FindFinancialService.ts
@@ -9,7 +9,7 @@ export class FindFinancialService {
   }
 
   public async execute(cod?: number) {
-    const query = this.conn('coalemos.financeiro')
+    const query_financeiro = this.conn('coalemos.financeiro')
       .select(
         'financeiro.cod',
         'financeiro.cod_cliente',
@@ -46,12 +46,55 @@ export class FindFinancialService {
         'coalemos.clientes as clientes',
         'clientes.nome',
         'financeiro.cod_cliente',
+      )
+      .where('financeiro.is_ativo', true);
+
+    const query_parcelas = this.conn('coalemos.financeiro_parcelas')
+      .where('financeiro_parcelas.is_ativo')
+      .select(
+        'financeiro_parcelas.cod',
+        'financeiro_parcelas.cod_financeiro',
+        'financeiro_parcelas.numero_parcela',
+        'financeiro_parcelas.data_vencimento',
+        'financeiro_parcelas.data_emissao',
+        'financeiro_parcelas.valor_original',
+        'financeiro_parcelas.valor_pago',
+        'financeiro_parcelas.desconto',
+        'financeiro_parcelas.acrescimo',
+        'financeiro_parcelas.status',
+        'financeiro_parcelas.data_pagamento',
+        'financeiro_parcelas.is_ativo',
+        'financeiro_parcelas.created_at',
+        'financeiro_parcelas.cod_usuario_criacao',
+        'financeiro_parcelas.updated_at',
+        'financeiro_parcelas.cod_usuario_updated',
+        'a2.nome as nome_usuario_updated',
+        'a1.nome as nome_usuario_criacao',
+      )
+      .leftJoin(
+        'coalemos.usuarios as a1',
+        'a1.cod',
+        'financeiro_parcelas.cod_usuario_criacao',
+      )
+      .leftJoin(
+        'coalemos.usuarios as a2',
+        'a2.cod',
+        'financeiro_parcelas.cod_usuario_updated',
       );
 
-    if (cod) query.where('financeiro.cod', cod).first();
+    if (cod) {
+      query_financeiro.where('financeiro.cod', cod).first();
+      query_parcelas.where('financeiro_parcelas.cod_financeiro', cod);
+    }
 
-    const result = await query;
+    const financeiro = await query_financeiro;
+    const parcelas = await query_parcelas;
 
-    return result;
+    for (let i = 0; i <= financeiro.length; i++)
+      financeiro[i].parcelas = parcelas.filter(
+        (l) => l.cod_financeiro == financeiro[i].cod,
+      );
+
+    return financeiro;
   }
 }

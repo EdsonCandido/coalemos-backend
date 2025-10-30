10dce5ca541693e649f2a03f0533d2bfec6cdac4
Edson Cândido <edson.c.a.j@hotmail.com>
2025-10-29 23:17:28 -0300

    [fix] config security polices and config seeds

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
-import "dotenv/config";
+import 'dotenv/config';
 import express from 'express';
 import compression from 'compression';
-import { createServer } from "http";
+import helmet from 'helmet';
+import { createServer } from 'http';
 
-import cors from "cors";
-import logger from "./middlewares/logger";
+import cors from 'cors';
+import logger from './middlewares/logger';
 import loggerMorgan from 'morgan';
-import env from "./configs/env";
-import routes from "./routes/index.routes";
-
+import env from './configs/env';
+import routes from './routes/index.routes';
+import { SeedsService } from './seeds';
 
 const APP_PORT = env.APP_PORT || 3333;
 
+const seedService = new SeedsService();
+seedService.execute();
+
 const app = express();
 app.disable('x-powered-by');
+app.use(helmet());
 app.use(compression());
 app.use(loggerMorgan('dev'));
 app.use(cors());
@@ -21,31 +26,30 @@ app.use(express.json({ limit: 12 * 1024 * 1024 }));
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-
 const http = createServer(app);
 
 enum ExitStatus {
-    falha = 1,
-    sucesso = 0,
+  falha = 1,
+  sucesso = 0,
 }
 
 http.listen(APP_PORT, () => {
-    try {
-        logger.info(`Servidor inicializado na porta ${APP_PORT}`);
-        const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
-        exitSignals.map((signal) =>
-            process.on(signal, async () => {
-                try {
-                    logger.info("Servidor desligado com sucesso.");
-                    process.exit(ExitStatus.sucesso);
-                } catch (error) {
-                    logger.error(`Error ao finalizar o servidor ${error}`);
-                    process.exit(ExitStatus.falha);
-                }
-            })
-        );
-    } catch (error) {
-        logger.error("Erro na inicialização do server: " + error);
-        process.exit(ExitStatus.falha);
-    }
+  try {
+    logger.info(`Servidor inicializado na porta ${APP_PORT}`);
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((signal) =>
+      process.on(signal, async () => {
+        try {
+          logger.info('Servidor desligado com sucesso.');
+          process.exit(ExitStatus.sucesso);
+        } catch (error) {
+          logger.error(`Error ao finalizar o servidor ${error}`);
+          process.exit(ExitStatus.falha);
+        }
+      }),
+    );
+  } catch (error) {
+    logger.error('Erro na inicialização do server: ' + error);
+    process.exit(ExitStatus.falha);
+  }
 });

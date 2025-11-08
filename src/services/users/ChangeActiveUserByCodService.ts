49c1b67a326c028da9726ccff1a40dc95ee332cf
Edson Cândido <edson.c.a.j@hotmail.com>
2025-11-07 21:33:00 -0300

    [refactor] change status is_active

--- src/services/users/ChangeActiveUserByCodService.ts
+++ src/services/users/ChangeActiveUserByCodService.ts
@@ -1,23 +1,29 @@
-import { Knex } from "knex";
-import pgConnection from "../../databases/pgConnection";
-import { FindUserByCodService } from "./FindUserByCodService";
-import { dtHoje } from "../../helpers/time";
+import { Knex } from 'knex';
+import pgConnection from '../../databases/pgConnection';
+import { FindUserByCodService } from './FindUserByCodService';
+import { dtHoje } from '../../helpers/time';
 
 export class ChangeActiveUserByCodService {
-    private conn: Knex<any, unknown>;
+  private conn: Knex<any, unknown>;
 
-    constructor() {
-        this.conn = pgConnection
-    }
-    public async execute(cod: number) {
+  constructor() {
+    this.conn = pgConnection;
+  }
+  public async execute(cod: number) {
+    if (!cod) throw new Error('Dados inválidos.');
 
-        if (!cod) throw new Error("Dados inválidos.");
+    const userExist = await this.conn('coalemos.usuarios')
+      .where({ cod })
+      .first('is_ativo');
 
-        await this.conn('coalemos.usuarios').where({ cod })
-            .update({ is_ativo: false, updated_at: dtHoje() });
+    if (!userExist) throw new Error('Usuário não encontrado!');
 
-        const service = new FindUserByCodService();
-        const result = await service.execute(cod);
-        return result;
-    }
+    await this.conn('coalemos.usuarios')
+      .where({ cod })
+      .update({ is_ativo: !userExist.is_ativo, updated_at: dtHoje() });
+
+    const service = new FindUserByCodService();
+    const result = await service.execute(cod);
+    return result;
+  }
 }
